984e532fb2029ae148487cf363e5241b6a6d2d8f
Kateb <kghemari@yahoo.com>
2018-12-19 15:34:45 +0100

    Fixing semver issues and putting the code to work

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -1,2 +1,7 @@
 
 /// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Dialogs.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Device.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Vibration.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\StatusBar.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\InAppBrowser.d.ts"/>
